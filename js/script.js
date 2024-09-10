eaca6faed5a8542f96d3b34477e23b93b97af233
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-09-10 12:50:01 +0100

    modified:   css/style.css 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ window.onscroll = function() {
     const scrollPosition = window.scrollY; // Get the scroll position on the Y-axis
 
     if (scrollPosition > 100) { // Adjust 100px to the point where you want to trigger the class
-        element.classList.add('fixed-top');
+        element.classList.add('sticky-top');
 
     } else {
-        element.classList.remove('fixed-top');
+        element.classList.remove('sticky-top');
     }
 };
