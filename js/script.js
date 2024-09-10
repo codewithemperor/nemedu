a9d18853eaf330a29620e69c980b6c5db4227b09
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-09-10 13:43:20 +0100

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-(function ($) {
-    "use strict";
+// (function ($) {
+//     "use strict";
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 40) {
-            $('.navbar').addClass('sticky-top');
-        } else {
-            $('.navbar').removeClass('sticky-top');
-        }
-    });
+//     // Sticky Navbar
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 40) {
+//             $('.navbar').addClass('sticky-top');
+//         } else {
+//             $('.navbar').removeClass('sticky-top');
+//         }
+//     });
     
-    // Dropdown on mouse hover
+//     // Dropdown on mouse hover
     
     
-})(jQuery);
+// })(jQuery);
