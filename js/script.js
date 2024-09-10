45fa089b85985ee4fb74a41b7467426093578c73
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-09-10 13:27:09 +0100

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 window.onscroll = function() {
     const element = document.querySelector('.navbar');
-    const scrollPosition = window.scrollY; // Get the scroll position on the Y-axis
+    const elementPosition = element.getBoundingClientRect().top; // Get element's position relative to the viewport
 
-    if (scrollPosition > 100) { // Adjust 100px to the point where you want to trigger the class
+    if (elementPosition <= 0) { // When the navbar reaches the top of the viewport
         element.classList.add('sticky-top');
-
     } else {
         element.classList.remove('sticky-top');
     }
